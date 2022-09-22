d5d8c65a52b4bc85d756b017c5baddcdd63d67c8
TikhampornSky <tikhamporntan@gmail.com>
2022-09-22 15:07:20 +0700

    First Draft

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  resources :scores
+  resources :students
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  get 'students/:id/edit_score', to: 'students#edit_score'
+end
